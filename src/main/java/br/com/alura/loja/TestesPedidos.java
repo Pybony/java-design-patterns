952924a203d0be8d0c4ddbbfbef6dbe503061f24
Lucas Maciel Schneider <lksbrs@gmail.com>
2023-02-02 16:16:52 -0300

    Observer

--- src/main/java/br/com/alura/loja/TestesPedidos.java
+++ src/main/java/br/com/alura/loja/TestesPedidos.java
@@ -2,18 +2,27 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class TestesPedidos {
 
-    public static void main(String[] args){
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+    public static void main(String[] args) {
+        //simulação entrada de dados
+        String cliente = "Rodrigo";
+        BigDecimal valorOrcamento = new BigDecimal("200");
+        int quantidadeItens = Integer.parseInt("4");
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(/*dependencias*/);
+
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                Arrays.asList(
+                        new SalvarPedidoNoBancoDeDados(),
+                        new EnviarEmailPedido()
+                ));
         handler.execute(gerador);
 
     }
